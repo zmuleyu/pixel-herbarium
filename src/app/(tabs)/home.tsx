82ff50c6893c4e1b8e6aad687bd5565db84122e8
zmuleyu <zmuleyu@gmail.com>
2026-03-29 14:38:17 +0800

    auto: agent checkpoint 14:38

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -10,7 +10,8 @@ import {
   Platform,
 } from 'react-native';
 import { useTranslation } from 'react-i18next';
-import { useRouter } from 'expo-router';
+import { useRouter, useFocusEffect } from 'expo-router';
+import { useCallback, useRef } from 'react';
 import { getActiveSeason } from '@/constants/seasons';
 import {
   colors,
